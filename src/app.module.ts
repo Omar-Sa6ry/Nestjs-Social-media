7f2de4a44f3fcbcc5f26b5bfe262f2a466139e52
Omar sabry <omarsabry@gmail.com>
2025-02-05 21:32:53 +0200

    updata app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,92 +1,31 @@
 import { Module } from '@nestjs/common'
 import { AppService } from './app.service'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { ThrottlerModule } from '@nestjs/throttler'
-import { join } from 'path'
-import { GraphQLModule } from '@nestjs/graphql'
-import { ApolloDriver } from '@nestjs/apollo'
-import { AppResolver } from './app.resolver'
 import { AuthModule } from './modules/auth/auth.module'
 import { UserModule } from './modules/users/users.module'
-import { User } from './modules/users/entity/user.entity'
-import { Notification } from './modules/notification/entity/notification.entity'
-import { Message } from './modules/message/entity/message.entity'
-import { Post } from './modules/post/entity/post.entity '
-import { Comment } from './modules/comment/entity/comment.entity '
-import { NotificationModule } from './modules/notification/notification.module'
 import { PostModule } from './modules/post/post.module'
 import { MessageModule } from './modules/message/message.module'
-import { RedisModule } from './common/redis/redis.module'
-import { Image } from './modules/post/entity/image.entity'
-import { UploadModule } from './common/upload/upload.module'
-import { Mention } from './modules/mention/entity/mention.entity '
 import { MentionModule } from './modules/mention/mention.module'
 import { LikeModule } from './modules/like/like.module'
-import { Like } from './modules/like/entity/like.entity '
 import { CommentModule } from './modules/comment/comment.module'
 import { ReplyModule } from './modules/reply/reply.module'
-import { Reply } from './modules/reply/entity/reply.entity '
 import { BlockModule } from './modules/Block/block.module'
-import { Block } from './modules/Block/entity/block.entity'
 import { RelationModule } from './modules/follow/follow.module'
-import { Follow } from './modules/follow/entity/follow.entity'
-import { Hashtag } from './modules/hastage/entity/hastage.entity'
 import { HashtagModule } from './modules/hastage/hashtage.module'
 import { GraphqlModule } from './common/graphql/graphql.module'
+import { ThrottlerModule } from './common/throttler/throttling.module'
+import { DataBaseModule } from './common/database/database'
+import { ConfigModule } from './common/config/config.module'
+import { AppResolver } from './app.resolver'
+import { NotificationModule } from './modules/notification/notification.module'
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ cache: true, isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [
-          User,
-          Notification,
-          Follow,
-          Block,
-          Message,
-          Post,
-          Image,
-          Comment,
-          Reply,
-          Mention,
-          Like,
-          Hashtag,
-        ],
-        synchronize: true,
-        logging: false,
-      }),
-      inject: [ConfigService],
-    }),
-    ThrottlerModule.forRoot([
-      {
-        name: 'short',
-        ttl: 1000,
-        limit: 3,
-      },
-      {
-        name: 'medium',
-        ttl: 10000,
-        limit: 20,
-      },
-      {
-        name: 'long',
-        ttl: 60000,
-        limit: 100,
-      },
-    ]),
+    ConfigModule,
     GraphqlModule,
-    RedisModule,
+    DataBaseModule,
+    ThrottlerModule,
+
     AuthModule,
-    UploadModule,
     UserModule,
     NotificationModule,
     PostModule,
---
+++ src/common/database/database.ts
@@ -0,0 +1,49 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Block } from 'src/modules/Block/entity/block.entity'
+import { Comment } from 'src/modules/comment/entity/comment.entity '
+import { Follow } from 'src/modules/follow/entity/follow.entity'
+import { Hashtag } from 'src/modules/hastage/entity/hastage.entity'
+import { Like } from 'src/modules/like/entity/like.entity '
+import { Mention } from 'src/modules/mention/entity/mention.entity '
+import { Message } from 'src/modules/message/entity/message.entity'
+import { Notification } from 'src/modules/notification/entity/notification.entity'
+import { Image } from 'src/modules/post/entity/image.entity'
+import { Post } from 'src/modules/post/entity/post.entity '
+import { Reply } from 'src/modules/reply/entity/reply.entity '
+import { User } from 'src/modules/users/entity/user.entity'
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [
+          User,
+          Notification,
+          Follow,
+          Block,
+          Message,
+          Post,
+          Image,
+          Comment,
+          Reply,
+          Mention,
+          Like,
+          Hashtag,
+        ],
+        synchronize: true,
+        logging: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DataBaseModule {}
